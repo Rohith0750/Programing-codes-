98485d140b4d71e7ff212f1befea7f87d7c9094f
Rohith S D <rohithsd0222@gmail.com>
2026-05-14 23:19:58 +0530

    Fast Book Lookup Using Hash Table

---
+++ LO_10_02.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class BookLookup {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+      
+        for (int i = 0; i < n; i++) {
+
+            String title = sc.next();
+            int id = sc.nextInt();
+
+            map.put(title, id);
+        }
+
+        int q = sc.nextInt();
+
+        
+        for (int i = 0; i < q; i++) {
+
+            String query = sc.next();
+
+            if (map.containsKey(query))
+                System.out.println(map.get(query));
+            else
+                System.out.println("Book Not Found");
+        }
+
+        sc.close();
+    }
+}
